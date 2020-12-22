a2c0042960befb34edc4db3b701ec9827159c8c7
Ahmad Danang <ahmad.danang91@gmail.com>
2020-12-22 13:51:39 +0700

    edit url sorting discover category

--- src/pages/DiscoverCategory.js
+++ src/pages/DiscoverCategory.js
@@ -46,24 +46,21 @@ const DiscoverCategory = () => {
 
   const sortUrgent = (e) => {
     e.preventDefault();
-    const urlDataUrgent =
-      "https://binar8-agus-saputra.nandaworks.com/campaigns/urgent";
+    const urlDataUrgent="https://binar8-agus-saputra.nandaworks.com/campaigns/urgent/more"
     axios.get(urlDataUrgent).then((res) => {
       setData(res.data.filter((data) => data.category === category));
     });
   };
   const sortLessDonate = (e) => {
     e.preventDefault();
-    const urlDataLess =
-      "https://binar8-agus-saputra.nandaworks.com/campaigns/less/donate";
+    const urlDataLess ="https://binar8-agus-saputra.nandaworks.com/campaigns/less/donate/more"
     axios.get(urlDataLess).then((res) => {
       setData(res.data.filter((data) => data.category === category));
     });
   };
   const sortPopular = (e) => {
     e.preventDefault();
-    const urlDataPupular =
-      "https://binar8-agus-saputra.nandaworks.com/campaigns/popular";
+    const urlDataPupular="https://binar8-agus-saputra.nandaworks.com/campaigns/popular/more"
     axios.get(urlDataPupular).then((res) => {
       setData(res.data.filter((data) => data.category === category));
     });
@@ -102,11 +99,9 @@ const DiscoverCategory = () => {
         </Row>
 
         <Row className="mt-5 mb-5">
-          <Col>
-            <Col className="d-flex justify-content-center">
-              {loading && <Loading type="spokes" color="#1D94A8" />}
-              {!loading && <CardCampaign data={data} />}
-            </Col>
+          <Col className="d-flex justify-content-center ">
+            {loading && <Loading type="spokes" color="#1D94A8" />}
+            {!loading && <CardCampaign data={data} />}
           </Col>
         </Row>
       </Container>
